dcd081dd47e953f681066c353e59eef9928d7d8c
Robert Porter <robert.porter@rakuten.com>
2020-10-29 23:15:52 +0900

    API simplification

--- src/Control/Monad/Resource/Aff/Pool.purs
+++ src/Control/Monad/Resource/Aff/Pool.purs
@@ -4,21 +4,13 @@ import Prelude
 import Control.Monad.Resource (class MonadResource)
 import Control.Monad.Resource as Resource
 import Data.Foldable (traverse_)
-import Data.Map (Map)
 import Data.Map as Map
-import Effect.Aff (Aff, Fiber)
+import Effect.Aff (Aff)
 import Effect.Aff as Aff
 import Effect.Class (liftEffect)
-import Effect.Ref (Ref)
 import Effect.Ref as Ref
 
-newtype Pool
-  = Pool
-  { fresh :: Ref Int
-  , fibers :: Ref (Map Int (Fiber Unit))
-  }
-
-create :: forall m. MonadResource m => m Pool
+create :: forall m a. MonadResource m => m (Aff a -> Aff a)
 create = do
   pool <-
     liftEffect ado
@@ -30,23 +22,20 @@ create = do
       fibers <- liftEffect $ Ref.modify' (\fibers -> { state: Map.empty, value: fibers }) pool.fibers
       traverse_ (Aff.killFiber (Aff.error "Cancelling")) fibers
   _ <- Resource.register cleanup
-  pure (Pool pool)
-
-run :: forall a. Pool -> Aff a -> Aff a
-run (Pool pool) aff = do
-  fiber <-
-    liftEffect do
-      id <- Ref.modify' (\fresh -> { state: fresh + 1, value: fresh }) pool.fresh
-      doneRef <- Ref.new false
-      fiber <-
-        Aff.launchAff
-          $ Aff.finally
-              ( liftEffect do
-                  Ref.modify_ (Map.delete id) pool.fibers
-                  Ref.write true doneRef
-              )
-              aff
-      unlessM (Ref.read doneRef) do
-        Ref.modify_ (Map.insert id (void fiber)) pool.fibers
-      pure fiber
-  Aff.joinFiber fiber
+  pure \aff -> do
+    fiber <-
+      liftEffect do
+        id <- Ref.modify' (\fresh -> { state: fresh + 1, value: fresh }) pool.fresh
+        doneRef <- Ref.new false
+        fiber <-
+          Aff.launchAff
+            $ Aff.finally
+                ( liftEffect do
+                    Ref.modify_ (Map.delete id) pool.fibers
+                    Ref.write true doneRef
+                )
+                aff
+        unlessM (Ref.read doneRef) do
+          Ref.modify_ (Map.insert id (void fiber)) pool.fibers
+        pure fiber
+    Aff.joinFiber fiber
