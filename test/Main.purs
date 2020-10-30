6a5efc99cc07e8ab5edfd43be7dd2842468ccc6a
Robert Porter <robert.porter@rakuten.com>
2020-10-30 12:37:17 +0900

    Added more tests

--- test/Main.purs
+++ test/Main.purs
@@ -7,7 +7,7 @@ import Data.Array as Array
 import Data.Map as Map
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import Effect.Aff (Aff, launchAff_)
+import Effect.Aff (Aff, Milliseconds(..), delay, launchAff_, parallel, sequential)
 import Effect.Aff.Class (liftAff)
 import Effect.Class (liftEffect)
 import Effect.Ref as Ref
@@ -60,6 +60,38 @@ main =
                 resource.expect.released [ Two ]
             resource.expect.pending []
             resource.expect.released [ Two, Three, One ]
+          it "should handle parallel resource acquisition" do
+            resource <- makeResource
+            Resource.runResource do
+              sequential ado
+                parallel do
+                  liftAff $ delay $ Milliseconds 500.0
+                  _ <- resource.register One
+                  pure unit
+                parallel do
+                  liftAff $ delay $ Milliseconds 200.0
+                  _ <- resource.register Two
+                  pure unit
+                in unit
+              _ <- resource.register Three
+              liftAff do
+                resource.expect.pending [ Tuple 0 Two, Tuple 1 One, Tuple 2 Three ]
+                resource.expect.released []
+            resource.expect.pending []
+            resource.expect.released [ Three, One, Two ]
+          it "should wait for forks to resolve before releasing" do
+            resource <- makeResource
+            Resource.runResource do
+              _ <- resource.register One
+              _ <- Resource.forkAff $ delay $ Milliseconds 500.0
+              liftAff do
+                resource.expect.pending [ Tuple 0 One ]
+                resource.expect.released []
+            resource.expect.pending [ Tuple 0 One ]
+            resource.expect.released []
+            delay $ Milliseconds 600.0
+            resource.expect.pending []
+            resource.expect.released [ One ]
 
 makeResource ::
   forall m.
